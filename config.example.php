52d9ed94f8d6dba274f1623a60eb19ad6c74514c
phate408 <phate408@gmail.com>
2014-10-31 01:52:56 -0700

    Added db_name config value and imported config file into index

--- config.example.php
+++ config.example.php
@@ -3,5 +3,6 @@
 $db_host = "localhost";
 $db_user = "user";
 $db_pass = "pass";
+$db_name = "parkingtracker";
 
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'config.php';
 
 $app = new \Slim\Slim();
-$db = new PDO("mysql:dbname=CS420G4;host={$db_host}", $db_user, $db_pass);
+$db = new PDO("mysql:dbname={$db_name};host={$db_host}", $db_user, $db_pass);
 
 $app->get('/', function () {
     echo 'Nothing to see here';
