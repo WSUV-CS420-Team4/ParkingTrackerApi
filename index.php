e7dcec1a6641ad1e21001f33ee4412abb3006c95
phate408 <phate408@gmail.com>
2014-11-17 16:22:56 -0800

    Parse blockfaces

--- index.php
+++ index.php
@@ -10,11 +10,45 @@ $app->get('/', function () {
     echo 'Nothing to see here';
 });
 
-$app->post('/upload', function () {
-    //We could store stuff here
+$app->get('/blockfaces', function () {
+  //Retrieve all? blockfaces
+  //Should probably paginate
 });
+
+$app->get('/blockfaces/:id', function($id) {
+  //Retrieve single blockface
+});
+
+$app->post('/blockfaces', function () {
+    $body = $app->request->getBody();
+    $data = json_decode($body);
+    if ($data === NULL) {
+      $app->response->setStatus(400);
+      return;
+    }
+    $insert = $db->prepare("INSERT INTO Parking (Plate, Block, Face, Time) VALUES (:plate, :block, :face, :time)");
+    $insert->bindParam(':block', $block);
+    $insert->bindParam(':face', $face);
+    $insert->bindParam(':plate', $plate);
+    $insert->bindParam(':time', $time);
+
+    foreach ($data->blockfaces as $blockface) {
+      $block = $blockface->block;
+      $face = $blockface->face
+      foreach ($blockface->stalls as $stall) {
+        if (strlen($stall->plate) > 0) {
+          $plate = $stall->plate;
+          $dt = DateTime::createFromFormat(DateTime::ISO8601, $stall->time);
+          $time = $dt->format("Y-m-d H:i:s");
+          $insert->execute();
+        } else {
+          //empty stall
+        }
+      }
+    }
+});
+
 
 $app->run();
 
 ?>
-
