9bb795598f799273b70cf4476153c41583fd87d1
phate408 <phate408@gmail.com>
2014-11-18 15:15:20 -0800

    Parking stall upload, and retreival

--- index.php
+++ index.php
@@ -10,34 +10,42 @@ $app->get('/', function () {
     echo 'Nothing to see here';
 });
 
-$app->get('/blockfaces', function () {
+$app->get('/blockfaces', function () use ($db) {
   //Retrieve all? blockfaces
   //Should probably paginate
+  $results = $db->query("SELECT Plate, Block, Face, Stall, Time FROM Parking ORDER BY Block, Face, Stall");
+  $data = $results->fetchAll(PDO::FETCH_OBJ);
+  echo json_encode($data, JSON_NUMERIC_CHECK);
 });
 
-$app->get('/blockfaces/:id', function($id) {
+$app->get('/blockfaces/:id', function($id) use ($db) {
   //Retrieve single blockface
+  $stmt = $db->prepare("SELECT Plate, Block, Face, Stall, Time FROM Parking WHERE ParkingId = :id");
+  $stmt->execute(array(":id" => $id));
+  echo json_encode($stmt->fetchObject("Stall"), JSON_NUMERIC_CHECK);
 });
 
-$app->post('/blockfaces', function () {
+$app->post('/blockfaces', function () use ($app, $db) {
     $body = $app->request->getBody();
     $data = json_decode($body);
     if ($data === NULL) {
       $app->response->setStatus(400);
       return;
     }
-    $insert = $db->prepare("INSERT INTO Parking (Plate, Block, Face, Time) VALUES (:plate, :block, :face, :time)");
+    $insert = $db->prepare("INSERT INTO Parking (Plate, Block, Face, Stall, Time) VALUES (:plate, :block, :face, :stall, :time)");
     $insert->bindParam(':block', $block);
     $insert->bindParam(':face', $face);
     $insert->bindParam(':plate', $plate);
+    $insert->bindParam(':stall', $stallNum);
     $insert->bindParam(':time', $time);
 
     foreach ($data->blockfaces as $blockface) {
       $block = $blockface->block;
       $face = $blockface->face;
-      foreach ($blockface->stalls as $stall) {
+      foreach ($blockface->stalls as $key => $stall) {
         if (strlen($stall->plate) > 0) {
           $plate = $stall->plate;
+          $stallNum = $key;
           $dt = DateTime::createFromFormat(DateTime::ISO8601, $stall->time);
           $time = $dt->format("Y-m-d H:i:s");
           $insert->execute();
@@ -48,7 +56,13 @@ $app->post('/blockfaces', function () {
     }
 });
 
-
+class Stall {
+    public $Plate;
+    public $Block;
+    public $Face;
+    public $Stall;
+    public $Time;
+}
 $app->run();
 
 ?>
