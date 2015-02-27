86399dbddc3ed81e7345e6de3623ff268950623d
Jason Moss <jason_moss@wsu.edu>
2015-02-26 16:34:12 -0800

    Fix indentation and add auth for blackfaces post

--- index.php
+++ index.php
@@ -26,34 +26,34 @@ $app->get('/blockfaces/:id', function($id) use ($db) {
 });
 
 $app->post('/blockfaces', function () use ($app, $db) {
-    //Upload parking data
-    $body = $app->request->getBody();
-    $data = json_decode($body);
-    if ($data === NULL) {
-      badRequest($app);
-      return;
-    }
-    $insert = $db->prepare("INSERT INTO Parking (Plate, Block, Face, Stall, Time) VALUES (:plate, :block, :face, :stall, :time)");
-    $insert->bindParam(':block', $block);
-    $insert->bindParam(':face', $face);
-    $insert->bindParam(':plate', $plate);
-    $insert->bindParam(':stall', $stallNum);
-    $insert->bindParam(':time', $time);
+  //Upload parking data
+  checkSession($app, $db);
+  $body = $app->request->getBody();
+  $data = json_decode($body);
+  if ($data === NULL) {
+    badRequest($app);
+    return;
+  }
+  $insert = $db->prepare("INSERT INTO Parking (Plate, Block, Face, Stall, Time) VALUES (:plate, :block, :face, :stall, :time)");
+  $insert->bindParam(':block', $block);
+  $insert->bindParam(':face', $face);
+  $insert->bindParam(':plate', $plate);
+  $insert->bindParam(':stall', $stallNum);
+  $insert->bindParam(':time', $time);
 
-    foreach ($data->blockfaces as $blockface) {
-      $block = $blockface->Block;
-      $face = $blockface->Face;
-      foreach ($blockface->Stalls as $key => $stall) {
-        if ((isset($stall->Plate)) && (strlen($stall->Plate) > 0)) {
-          $plate = $stall->Plate;
-          $stallNum = $key;
-          $dt = DateTime::createFromFormat(DateTime::ISO8601, $stall->Time);
-          $time = $dt->format("Y-m-d H:i:s");
-          $insert->execute();
-        } else {
-          //empty stall
-          
-        }
+  foreach ($data->blockfaces as $blockface) {
+    $block = $blockface->Block;
+    $face = $blockface->Face;
+    foreach ($blockface->Stalls as $key => $stall) {
+      if ((isset($stall->Plate)) && (strlen($stall->Plate) > 0)) {
+        $plate = $stall->Plate;
+        $stallNum = $key;
+        $dt = DateTime::createFromFormat(DateTime::ISO8601, $stall->Time);
+        $time = $dt->format("Y-m-d H:i:s");
+        $insert->execute();
+      } else {
+        //empty stall
+        
       }
     }
 });
